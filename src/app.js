8b6f57bbd85ced817ca42dbfcb345efb899b046a
langtungheo <langtungheo093@gmail.com>
2021-10-19 09:27:06 +0700

    fix cors

--- src/app.js
+++ src/app.js
@@ -9,8 +9,16 @@ const port = process.env.PORT || 8000;
 const app = express();
 const pathPublish = path.join(__dirname, "../publish");
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
 app.use(express.json())
-app.use(cors())
+app.use(allowCrossDomain)
 app.use("/publish",express.static(pathPublish));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "./views"));
